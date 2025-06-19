c5a739b6ad8c5f1510e9ee7eb1126dfbaa71d559
tj408122155@163.com <tj408122155@163.com>
2025-06-19 18:32:40 +0800

    init

---
+++ src/main/java/com/spring/ai/example/client/SingleModelChatClientExample.java
@@ -0,0 +1,15 @@
+package com.spring.ai.example.client;
+
+
+/**
+ * @fileName SingleModelChatClientExample
+ * @description:
+ * @author: tj
+ * @date 2025年06月19日 17:58
+ */
+public class SingleModelChatClientExample {
+
+
+
+
+}
---
+++ src/test/java/com/spring/ai/example/SpringAiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.ai.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
